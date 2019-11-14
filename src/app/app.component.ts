0e5bd45560e1d6dd70822959c45222b5cddf463b
Wesley Kenyon <wesjkenyon@gmail.com>
2019-11-13 23:13:30 -0500

    Initial dark and light theme work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Wesley Kenyon';
+export class AppComponent implements OnInit
+{
+    title = 'Wesley Kenyon';
+
+    constructor() { }
+
+    ngOnInit()
+    {
+    }
+
+    onThemeChanged($event: MatSlideToggleChange): void {
+        if ($event.checked)
+        {
+            // document.getElementById('themeAsset').href = `/path/to/my/${themeName}.css`;
+        }
+        else
+        {
+            // document.getElementById('themeAsset').href = `/path/to/my/${themeName}.css`;
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
     declarations: [
@@ -16,7 +20,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         AppRoutingModule,
         ComponentsModule,
         BrowserAnimationsModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent]
